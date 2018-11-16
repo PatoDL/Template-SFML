2788fb8abaf8124228b0d89286d6588e0072e194
unknown <pehuen-testa@hotmail.com>
2018-11-16 09:34:18 -0300

    corregido el proyecto, anda el .bat

---
+++ Template-SFML/Template-SFML/copiador.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set solutiondir=%1
+set target=%2
+
+echo Copiando archivos...
+
+xcopy "%solutiondir%libs\SFML-2.5.1\bin\sfml-window-d-2.dll" "%target%" /E /Y /S
+xcopy "%solutiondir%libs\SFML-2.5.1\bin\sfml-system-d-2.dll" "%target%" /E /Y /S
+xcopy "%solutiondir%libs\SFML-2.5.1\bin\sfml-graphics-d-2.dll" "%target%" /E /Y /S
+
+echo Copia finalizada
