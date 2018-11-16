7b5410ac1155837cba07d5bdc2576c09ba0582cf
unknown <pehuen-testa@hotmail.com>
2018-11-16 09:47:24 -0300

    agregado el tile

--- Template-SFML/Template-SFML/copiador.bat
+++ Template-SFML/Template-SFML/copiador.bat
@@ -8,5 +8,7 @@ echo Copiando archivos...
 xcopy "%solutiondir%libs\SFML-2.5.1\bin\sfml-window-d-2.dll" "%target%" /E /Y /S
 xcopy "%solutiondir%libs\SFML-2.5.1\bin\sfml-system-d-2.dll" "%target%" /E /Y /S
 xcopy "%solutiondir%libs\SFML-2.5.1\bin\sfml-graphics-d-2.dll" "%target%" /E /Y /S
+xcopy "%solutiondir%libs\STP\bin\STP-d.dll" "%target%" /E /Y /S
+xcopy "%solutiondir%libs\STP\bin\zlibd.dll" "%target%" /E /Y /S
 
 echo Copia finalizada
--- Template-SFML/Template-SFML/main.cpp
+++ Template-SFML/Template-SFML/main.cpp
@@ -1,23 +1,79 @@
-#include <SFML/Graphics.hpp>
+#include "SFML/Graphics.hpp"
+#include "STP/TMXLoader.hpp"
+#include "SFML\Graphics\Texture.hpp"
+#include "SFML\Graphics\Sprite.hpp"
+
+using namespace sf;
+
+Texture texture;
+Sprite sprite;
+
+static void cargarTextura()
+{
+	texture.loadFromFile("res/xd.png");
+	sprite.setTexture(texture);
+}
+
+static void setSpriteConf()
+{
+	sprite.setPosition(90.f, 90.f);
+}
+
+void initSprite()
+{
+	cargarTextura();
+	setSpriteConf();
+}
+
+Sprite drawSprite()
+{
+	return sprite;
+}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "STP Example");
+	tmx::TileMap map("res/atr.tmx");
 
-	while (window.isOpen())
-	{
+	map.ShowObjects(); // Display all the layer objects.
+
+	float recY = 0.f;
+
+	sf::View view1;
+
+	sf::View view2(sf::Vector2f(400.f, 300.f), sf::Vector2f(800.f, 600.f));
+
+	while (window.isOpen()) {    // Start the game loop
+								 // Process events
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
+		while (window.pollEvent(event)) {
+			// Close window : exit
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
+		view1.reset(sf::FloatRect(0.f, recY, 800.f, 600.f));
+
+		window.setView(view1);
+		sf::View currentview = window.getView();
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			recY++;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			recY--;
+		}
+
+		// Clear screen
 		window.clear();
-		window.draw(shape);
+		// Draw the map
+		window.draw(map);
+		window.draw(drawSprite());
+		// Update the window
 		window.display();
 	}
-	return 0;
+
+	return EXIT_SUCCESS;
 }
